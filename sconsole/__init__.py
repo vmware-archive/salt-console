169adc829e0aea814640db4032270f3e3a40b36e
Thomas S Hatch <thatch45@gmail.com>
2013-11-27 23:23:39 -0700

    Set up gateway

--- sconsole/__init__.py
+++ sconsole/__init__.py
@@ -1,3 +1,8 @@
-'''
-Initialize the salt console
-'''
+# Import sconsole libs
+import sconsole.manager
+import sconsole.parser
+
+def start():
+    opts = sconsole.parser.parse()
+    manager = sconsole.manager.Manager(opts)
+    manager.start()
