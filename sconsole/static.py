72c4a1285eac5f70dfae243e911e0ab3c540d870
Thomas S Hatch <thatch45@gmail.com>
2013-11-30 13:15:45 -0700

    Add a logmsg utility function

--- sconsole/static.py
+++ sconsole/static.py
@@ -1,6 +1,17 @@
 '''
 Holds static data components, like the palette
 '''
+
+def msg(msg, logfile='console_log.txt'):
+    '''
+    Send a message to a logfile, defaults to console_log.txt.
+    This is useful to replace a print statement since curses does put
+    a bit of a damper on this
+    '''
+    with open(logfile, 'a+') as fp_:
+        fp_.write(str(msg))
+
+
 def get_palette(theme='std'):
     '''
     Return the preferred palette theme
