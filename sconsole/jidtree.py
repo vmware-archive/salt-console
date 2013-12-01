32d522415a4d0b1e374f8b289a0bc20c01a53719
Thomas S Hatch <thatch45@gmail.com>
2013-12-01 00:10:38 -0700

    Add hooks to handle the tree data format

--- sconsole/jidtree.py
+++ sconsole/jidtree.py
@@ -14,7 +14,15 @@ class JIDWidget(urwid.TreeWidget):
     Manage the display of the job return data
     '''
     def get_display_text(self):
-        return 'Job ID'
+        value = self.get_node().get_value()
+        if 'jids' in value:
+            return 'Job IDS'
+        elif value.keys()[0].startswith('_|-'):
+            return value.keys()[0]
+        elif not value.keys()[0].startswith('_|-') and 'return' not in value:
+            return value.keys()[0]
+        else:
+            return str(value['return'])
 
 
 class JIDNode(urwid.TreeNode):
@@ -32,17 +40,28 @@ class JIDParent(urwid.ParentNode):
         return JIDWidget(self)
 
     def load_child_keys(self):
-        data = self.get_value()
-        return range(len(data))
+        value = self.get_value()
+        if 'jids' in value:
+            keys = range(len(value['jids']))
+            return keys
+        elif value.keys()[0].startswith('_|-'):
+            keys = range(len(value[value.keys()[0]]))
+            return keys
+        elif not value.keys()[0].startswith('_|-') and 'return' not in value:
+            return range(len(value))
+        return range(len(value))
 
     def load_child_node(self, key):
         value = self.get_value()
         if 'jids' in value:
             data = value['jids'][key]
             childclass = JIDParent
-        if 'minions' in value:
-            data = value['minions'][key]
+        elif value.keys()[0].startswith('_|-'):
+            data = value[value.keys()[0]][key]
             childclass = JIDParent
+        elif not value.keys()[0].startswith('_|-') and 'return' not in value:
+            data = value[value.keys()[0]]
+            childclass = JIDNode
         else:
             data = value
             childclass = JIDNode
@@ -51,9 +70,11 @@ class JIDParent(urwid.ParentNode):
 
 class JIDView(object):
     def __init__(self):
-        self.treetop = JIDParent({})
+        self.treetop = JIDParent(sconsole.static.tree_seed())
         self.listbox = urwid.TreeListBox(urwid.TreeWalker(self.treetop))
         self.listbox.offset_rows = 1
 
     def update(self, loop, user_data=None):
         pass
+        #self.treetop.set_child_node(JIDNode)
+        #sconsole.static.msg(dir(self.treetop))
